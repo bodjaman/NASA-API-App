800d2460f7bc0aea0b2cff072aebc1f7e24294f7
bodjaman <48054212+bodjaman@users.noreply.github.com>
2020-08-11 16:57:58 +0100

    trying to deploy...

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let landed = new Date("2012-08-06"); // landing date
 let current = new Date(); // current date
 
 let generated, generatedYear, generatedMonth, generatedDay, generatedDateStr;
-const apiKey = process.env.API_KEY;
+const apiKey = "6ItzpbwnWxj8ZbItja9hE9UbkvqP1HiDSpFFNg2q";
 let url;
 
 function randomDateInRange(start, end) {
