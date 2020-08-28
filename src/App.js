6581852254aacbfe6012e2bbbcf2b8ac20b452b2
bodjaman <48054212+bodjaman@users.noreply.github.com>
2020-08-28 22:32:01 +0100

    implemented dynamic rendering, css, date limit

--- src/App.js
+++ src/App.js
@@ -5,27 +5,11 @@ import SelectDateAndRender from "./components/dateselect.component";
 import Header from "./components/header.component";
 
 class App extends React.Component {
-  // state = {
-  //   date: "",
-  // };
-
-  // selectDateHandler = (event) => {
-  //   this.setState({
-  //     date: event.target.value,
-  //   });
-  // };
-
   render() {
-    // const apiKey = "DEMO_KEY"; // CHANGE TO ENV VARIABLE
     return (
       <div className="container">
         <Header />
-        <br />
-        <SelectDateAndRender
-        // change={this.selectDateHandler}
-        // date={this.state.date}
-        // url={`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${this.state.date}&api_key=${apiKey}`}
-        />
+        <SelectDateAndRender />
       </div>
     );
   }
